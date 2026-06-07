666dbd788d01502eda2fd40d605c540b23546476
Kaja <takzwanahere@gmail.com>
2026-06-08 01:54:16 +0200

    Added JWT validation and endpoint protection using spring security

--- tts/src/main/java/com/timetracking/service/JwtService.java
+++ tts/src/main/java/com/timetracking/service/JwtService.java
@@ -1,7 +1,8 @@
 package com.timetracking.service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
@@ -11,21 +12,40 @@ import java.util.Date;
 @Service
 public class JwtService {
 
-    // tymczasowy sekret do developmentu
     private static final String SECRET =
             "supersecretkeysupersecretkeysupersecretkey12";
 
     private final SecretKey key = Keys.hmacShaKeyFor(SECRET.getBytes());
 
     public String generateToken(String username) {
-
         long expirationTime = 1000 * 60 * 60 * 24; // 24h
 
         return Jwts.builder()
                 .subject(username)
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + expirationTime))
-                .signWith(key, SignatureAlgorithm.HS256)
+                .signWith(key)
                 .compact();
+    }
+
+    public String extractUsername(String token) {
+        return extractClaims(token).getSubject();
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            Claims claims = extractClaims(token);
+            return claims.getExpiration().after(new Date());
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    private Claims extractClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
     }
 }
